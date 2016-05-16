4564fa15b2652a0ad7a570ad8e6cbe514aafd345
neilkidd <neil.a.kidd@gmail.com>
2016-05-16 15:25:45 +0100

    fixed apt-get duplicate warning with help from: http://serverfault.com/questions/376392/apt-get-duplicate-sources-list-entry-for-google-chrome-ubuntu-11-10

--- install_google_chrome.sh
+++ install_google_chrome.sh
@@ -8,7 +8,8 @@ function install_google_chrome {
     
     if [ ! -f /etc/apt/sources.list.d/google.list ]; then #consider nothing is configured
       wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-      sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+	  # http://serverfault.com/questions/376392/apt-get-duplicate-sources-list-entry-for-google-chrome-ubuntu-11-10
+      sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
     fi
     
     apt-get -qq update
